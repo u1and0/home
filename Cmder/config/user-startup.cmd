d3ff38b593b8a337ac9110a6e6cae296cdc736c3
U1and0 <e01.ando60@gmail.com>
2016-10-01 12:25:29 +0900

    add cmder setting

---
+++ Cmder/config/user-startup.cmd
@@ -0,0 +1,8 @@
+:: use this file to run your own startup commands 
+:: use @ in front of the command to prevent printing the command
+ 
+:: @call "C:\Program Files\Cmder\vendor\git-for-windows/cmd/start-ssh-agent.cmd
+:: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
+ 
+@prompt $E[1;32;40m$P$S{git}{hg}$S$_$E[1;30;40m$$$S$E[0m
+::λを$マークに変更
