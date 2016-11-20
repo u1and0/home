ee198ff967b813a8bf9c62e93dab8e09ebb89751
U1and0 <e01.ando60@gmail.com>
2016-11-20 23:45:47 +0900

    jupyter config modify latex

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -520,10 +520,14 @@ def post_save(model, os_path, contents_manager):
     if model['type'] != 'notebook':
         return # only do this for notebooks
     d, fname = os.path.split(os_path)
+    base, ext = os.path.splitext(fname)
     check_call(['jupyter', 'nbconvert', '--to', 'script', fname], cwd=d)
     check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
     check_call(['jupyter', 'nbconvert', '--to', 'markdown', fname], cwd=d)
-    check_call(['jupyter', 'nbconvert', '--to', 'latex', '--template', 'jsarticle.tplx', 'lualatex', fname], cwd=d)
+    check_call(['jupyter-nbconvert', '--to', 'latex', fname, '--template', 'jsarticle.tplx'], cwd=d)
+    check_call(['extractbb', base+'_files/*.png'], cwd=d)
+    check_call(['platex', '-interaction=nonstopmode', '-synctex=1', '-kanji=utf8', '-guess-input-enc' , base+'.tex'], cwd=d)
+    check_call(['dvipdfmx', base+'.dvi'], cwd=d)
 
 c.FileContentsManager.post_save_hook = post_save
 
@@ -574,19 +578,3 @@ c.FileContentsManager.post_save_hook = post_save
 #  
 #  By default, all installed kernels are allowed.
 #c.KernelSpecManager.whitelist = set()
-
-import os
-from subprocess import check_call
-
-def post_save(model, os_path, contents_manager):
-    """post-save hook for converting notebooks to .py scripts
-    ipynbが保存されるたびに[py, html, md, tex, pdf]を作成する。"""
-    if model['type'] != 'notebook':
-        return # only do this for notebooks
-    d, fname = os.path.split(os_path)
-    check_call(['jupyter', 'nbconvert', '--to', 'script', fname], cwd=d)
-    check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
-    check_call(['jupyter', 'nbconvert', '--to', 'markdown', fname], cwd=d)
-    check_call(['jupyter', 'nbconvert', '--to', 'latex', '--template', 'jsarticle.tplx', 'lualatex', fname], cwd=d)
-
-c.FileContentsManager.post_save_hook = post_save
