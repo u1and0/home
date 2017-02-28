1a9dd6a7437e9955763eccee6f36bf7ce53a89f1
U1and0 <e01.ando60@gmail.com>
2017-02-28 14:08:38 -0500

    <Add> @sublimetext: OmniMarkupPreviewer

---
+++ SublimeText3/Packages/OmniMarkupPreviewer/OmniMarkupLib/Renderers/libs/mdx_strikeout.py
@@ -0,0 +1,20 @@
+import markdown
+from markdown.inlinepatterns import SimpleTagPattern
+
+
+STRIKEOUT_RE = r'(\~\~)([^\s](?:.*))(\~\~)'
+
+
+class StrikeoutExtension(markdown.Extension):
+    """ Strikeout extension for Python-Markdown. """
+
+    def extendMarkdown(self, md, md_globals):
+        """ Modifies inline patterns. """
+        md.inlinePatterns.add('del', SimpleTagPattern(STRIKEOUT_RE, 'del'), '<not_strong')
+
+
+def makeExtension(*args, **kwargs):
+    """変更前
+    def makeExtension(configs=None):
+    return StrikeoutExtension(configs=configs) """
+    return StrikeoutExtension(*args, **kwargs)
