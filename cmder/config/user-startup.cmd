3ae591c2b849ff895b4bb17e60cd9121c8f8c459
U1and0 <e01.ando60@gmail.com>
2017-03-12 14:50:04 -0400

    cmder 129 rollback

--- cmder/config/user-startup.cmd
+++ cmder/config/user-startup.cmd
@@ -3,3 +3,10 @@
 
 :: @call "C:\Program Files\Cmder\vendor\git-for-windows/cmd/start-ssh-agent.cmd
 :: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
+
+@prompt $E[1;32;40m$P$S{git}{hg}$S$_$E[1;30;40m$$$S$E[0m
+::λを$マークに変更
+
+@chcp 65001
+
+cls
